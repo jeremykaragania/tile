58a7052127109304315da328545964b1db0f0587
Jeremy Karagania <jeremykaragania@icloud.com>
2024-02-23 12:04:53 +0800

    Update console helpers

--- tile/kernel/console.c
+++ tile/kernel/console.c
@@ -1,11 +1,13 @@
 #include <console.h>
 
-int console_putchar(int c) {
-  *(uint32_t*)VE_UART0 = c;
+volatile uint32_t* ve_uart0 = (volatile uint32_t*)0x1c090000;
+
+int console_putchar(const int c) {
+  *ve_uart0 = c;
   return c;
 }
 
-int console_puts(char* s) {
+int console_puts(const char* s) {
   size_t i = 0;
   while (s[i]) {
     console_putchar(s[i]);
--- tile/kernel/console.h
+++ tile/kernel/console.h
@@ -1,13 +1,11 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
-#define VE_UART0 0x1c090000
-
 #include <stddef.h>
 #include <stdint.h>
 
-int console_putchar(int c);
+int console_putchar(const int c);
 
-int console_puts(char* s);
+int console_puts(const char* s);
 
 #endif
