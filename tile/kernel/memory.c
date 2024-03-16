03b5a1a373ffd2a6d4decb743de28e020c8b8ea0
Jeremy Karagania <jeremykaragania@icloud.com>
2024-03-16 13:24:30 +0800

    Fix memory management helpers

--- tile/kernel/memory.c
+++ tile/kernel/memory.c
@@ -1,9 +1,9 @@
 #include <memory.h>
 
 uint32_t virt_to_phys(uint32_t x) {
-  return x + (VIRT_OFFSET - PHYS_OFFSET);
+  return x - ((uint32_t)&VIRT_OFFSET - (uint32_t)&PHYS_OFFSET);
 }
 
 uint32_t phys_to_virt(uint32_t x) {
-  return x - (VIRT_OFFSET - PHYS_OFFSET);
+  return x + ((uint32_t)&VIRT_OFFSET - (uint32_t)&PHYS_OFFSET);
 }
--- tile/kernel/memory.h
+++ tile/kernel/memory.h
@@ -1,13 +1,11 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-#define PHYS_OFFSET 0x80000000
-#define VIRT_OFFSET 0xC0000000
-#define TEXT_OFFSET 0x00008000
-#define KERNEL_RAM_PADDR PHYS_OFFSET + TEXT_OFFSET
-#define KERNEL_RAM_VADDR VIRT_OFFSET + TEXT_OFFSET
-
 #include <stdint.h>
+
+const extern uint32_t* PHYS_OFFSET;
+
+const extern uint32_t* VIRT_OFFSET;
 
 uint32_t virt_to_phys(uint32_t x);
 
