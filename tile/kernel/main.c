a1d5ea48aa6ff342c81aba61bb33a78ae4f4dbf3
Jeremy Karagania <jeremykaragania@icloud.com>
2024-03-16 10:50:41 +0800

    Add page table pointer

--- tile/kernel/main.c
+++ tile/kernel/main.c
@@ -1,9 +1,14 @@
 #include <process.h>
 
+const extern uint32_t* SMC_VADDR;
+const extern uint32_t* KERNEL_SPACE_VADDR;
+const extern uint32_t* PG_DIR_VADDR;
+
 extern void* init_end;
 extern void enable_interrupts();
 extern void disable_interrupts();
 
+static uint32_t* pg_dir = (uint32_t*)&PG_DIR_VADDR;
 static struct process_table_entry init_process_table_entry __attribute__((section(".init_process_table_entry"))) = {0, CREATED, &init_end};
 
 void start_kernel() {
