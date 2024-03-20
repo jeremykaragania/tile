dccfbe4d048103b7cee573864fa0c04f613c126a
Jeremy Karagania <jeremykaragania@icloud.com>
2024-03-20 13:13:56 +0800

    Add memory information

--- tile/kernel/memory.h
+++ tile/kernel/memory.h
@@ -1,11 +1,21 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 const extern uint32_t* PHYS_OFFSET;
 
 const extern uint32_t* VIRT_OFFSET;
+
+struct memory_info {
+  const uint32_t* text_begin;
+  const uint32_t* text_end;
+  const uint32_t* data_begin;
+  const uint32_t* data_end;
+  const uint32_t* bss_begin;
+  const uint32_t* bss_end;
+};
 
 uint32_t virt_to_phys(uint32_t x);
 
