49844062557f7e1647af2be113c90bebd02ea4fd
Jeremy Karagania <jeremykaragania@icloud.com>
2024-03-27 16:56:35 +0800

    Update memory information

--- tile/kernel/memory.c
+++ tile/kernel/memory.c
@@ -1,1 +1,21 @@
 #include <memory.h>
+
+static struct memory_region reserved_memory_region = {
+  0x0,
+  NULL
+};
+
+static struct memory_block memory_memory_blocks = {
+  0x80000000,
+  0x80000000
+};
+
+static struct memory_region memory_memory_region = {
+  0x80000000,
+  &memory_memory_blocks
+};
+
+struct memory_info memory_info = {
+  &reserved_memory_region,
+  &memory_memory_region
+};
--- tile/kernel/memory.h
+++ tile/kernel/memory.h
@@ -1,18 +1,29 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 const extern uint32_t* PHYS_OFFSET;
 
 const extern uint32_t* VIRT_OFFSET;
 
-struct memory_info {
-  const uint32_t text_begin;
-  const uint32_t text_end;
-  const uint32_t data_begin;
-  const uint32_t data_end;
+struct memory_block {
+  uint32_t begin;
+  uint32_t size;
 };
+
+struct memory_region {
+  unsigned long length;
+  struct memory_block* blocks;
+};
+
+struct memory_info {
+  struct memory_region* memory;
+  struct memory_region* reserved;
+};
+
+extern struct memory_info memory_info;
 
 extern uint32_t virt_to_phys(uint32_t x);
 
