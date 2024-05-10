a10d24e77c390a4e3b369c8d5e6ec29dfc8d13c0
Jeremy Karagania <jeremykaragania@icloud.com>
2024-05-10 13:10:48 +0800

    Add page table entry allocation and insertion

--- tile/kernel/page.c
+++ tile/kernel/page.c
@@ -7,7 +7,7 @@
   a virtual address "addr", and a translation table base, "pgd".
 */
 uint32_t* pgd_offset(uint32_t* pgd, uint32_t addr) {
-  return pgd + pgd_index(addr);
+  return (uint32_t*)((uint32_t)pgd + (uint32_t)pgd_index(addr));
 }
 
 /*
@@ -17,5 +17,33 @@ uint32_t* pgd_offset(uint32_t* pgd, uint32_t addr) {
   "addr" and a page table base address, "pte".
 */
 uint32_t* pte_offset(uint32_t* pte, uint32_t addr) {
-  return pte + pte_index(addr);
+  return (uint32_t*)((uint32_t)pte + (uint32_t)pte_index(addr));
+}
+
+/*
+  pte_alloc allocates a new page table entry from a virtual address "addr" and
+  returns a pointer to it. The appropriate index in the page global directory
+  specified by the memory manager "mm" is updated to point to the new page
+  table entry.
+*/
+uint32_t* pte_alloc(struct memory_manager* mm, uint32_t addr) {
+  uint32_t* pte;
+  uint32_t* offset;
+  pte = memory_alloc(PAGE_SIZE);
+  offset = pgd_offset(mm->pg_dir, addr);
+  *offset = ((uint32_t)pte & 0xfffffc00) | 0x1;
+  invalidate_entire_tlb();
+  return pte;
+}
+
+/*
+  pte_insert inserts a page into a page table entry "pte". The page is
+  specified by which physical address "p_addr" it maps to which virtual address
+  "v_addr". If a page already exists it is replaced.
+*/
+void pte_insert(uint32_t* pte, uint32_t v_addr, uint32_t p_addr) {
+  uint32_t* offset;
+  offset = pte_offset(pte, v_addr);
+  *offset = ((uint32_t)p_addr & 0xfffff000) | 0x1 << 0x4 | 0x1 << 0x1;
+  invalidate_entire_tlb();
 }
--- tile/kernel/page.h
+++ tile/kernel/page.h
@@ -2,6 +2,7 @@
 #define PAGE_H
 
 #include <stdint.h>
+#include <memory.h>
 
 extern uint32_t pgd_index(uint32_t addr);
 extern uint32_t pte_index(uint32_t addr);
@@ -9,5 +10,7 @@ extern void invalidate_entire_tlb();
 
 uint32_t* pgd_offset(uint32_t* pgd, uint32_t addr);
 uint32_t* pte_offset(uint32_t* pte, uint32_t addr);
+uint32_t* pte_alloc(struct memory_manager* mm, uint32_t addr);
+void pte_insert(uint32_t* pte, uint32_t v_addr, uint32_t p_addr);
 
 #endif
