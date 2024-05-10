1d80930b8a22d3aa79cb01a10c00d90fe99bba7a
Jeremy Karagania <jeremykaragania@icloud.com>
2024-05-10 13:13:38 +0800

    Add page table entry for the static memory controller

--- tile/kernel/main.c
+++ tile/kernel/main.c
@@ -1,16 +1,25 @@
 #include <asm/memory.h>
 #include <memory.h>
+#include <page.h>
 #include <process.h>
 #include <uart.h>
 
 extern void enable_interrupts();
 extern void disable_interrupts();
 
+void map_smc() {
+  uint32_t* pte;
+  pte = pte_alloc(&memory_manager, 0Xffc00000);
+  pte_insert(pte, 0xffc00000, 0x1c090000);
+  return;
+}
+
 void start_kernel() {
   disable_interrupts();
   set_process_stack_end_token(&init_process);
   init_memory_map();
   init_memory_manager((uint32_t*)PG_DIR_PADDR, &text_begin, &text_end, &data_begin, &data_end);
+  map_smc();
   uart_init();
   return;
 }
