e93c1ee3ce2140218299308e2a86333fe2573f9d
Jeremy Karagania <jeremykaragania@icloud.com>
2024-05-26 19:17:40 +0800

    Update compiler integer division helpers

--- tile/kernel/helpers.c
+++ tile/kernel/helpers.c
@@ -11,7 +11,7 @@ int __aeabi_idiv(int numerator, int denominator) {
     sign ^= 1;
     numerator = -numerator;
   }
-  q = __aeabi_uidiv(numerator, denominator);
+  q = divide(numerator, denominator).quotient;
   if (sign) {
     q = -q;
   }
@@ -19,10 +19,17 @@ int __aeabi_idiv(int numerator, int denominator) {
 }
 
 unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator) {
-  int q = 0;
-  while (numerator >= denominator) {
-    numerator -= denominator;
-    q += 1;
+  return divide(numerator, denominator).quotient;
+}
+
+struct divide_return divide(unsigned long long numerator, unsigned long long denominator) {
+  struct divide_return ret = {
+    0,
+    numerator
+  };
+  while (ret.remainder >= denominator) {
+    ret.remainder -= denominator;
+    ret.quotient += 1;
   }
-  return q;
+  return ret;
 }
--- tile/kernel/helpers.h
+++ tile/kernel/helpers.h
@@ -1,6 +1,13 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+struct divide_return {
+  unsigned long long quotient;
+  unsigned long long remainder;
+};
+
+struct divide_return divide(unsigned long long numerator, unsigned long long denominator);
+
 int __aeabi_idiv(int numerator, int denominator);
 unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator);
 
