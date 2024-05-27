e5c38397bcb3ac32b07b4c71fbe9c28d148b2441
Jeremy Karagania <jeremykaragania@icloud.com>
2024-05-27 18:48:25 +0800

    Add compiler long long division helpers

--- tile/kernel/helpers.c
+++ tile/kernel/helpers.c
@@ -1,45 +1,69 @@
 #include <helpers.h>
 
-struct divide_return divide(unsigned long long numerator, unsigned long long denominator) {
-  struct divide_return ret = {
-    0,
-    numerator
-  };
-  while (ret.remainder >= denominator) {
-    ret.remainder -= denominator;
-    ret.quotient += 1;
-  }
-  return ret;
-}
-
-int __aeabi_idiv(int numerator, int denominator) {
+static struct signed_divide_return signed_divide(long long numerator, long long denominator) {
   int sign = 0;
-  int q;
+  struct unsigned_divide_return udr;
+  struct signed_divide_return sdr;
+
   if (denominator < 0) {
     sign ^= 1;
     denominator = -denominator;
   }
+
   if (numerator < 0) {
     sign ^= 1;
     numerator = -numerator;
   }
-  q = divide(numerator, denominator).quotient;
+
+  udr = unsigned_divide(numerator, denominator);
+  sdr.quotient = udr.quotient;
+  sdr.remainder = udr.remainder;
+
   if (sign) {
-    q = -q;
+    sdr.quotient = -sdr.quotient;
   }
-  return q;
+
+  return sdr;
+}
+
+static struct unsigned_divide_return unsigned_divide(unsigned long long numerator, unsigned long long denominator) {
+  struct unsigned_divide_return udr = {
+    0,
+    numerator
+  };
+
+  while (udr.remainder >= denominator) {
+    udr.remainder -= denominator;
+    udr.quotient += 1;
+  }
+
+  return udr;
+}
+
+int __aeabi_idiv(int numerator, int denominator) {
+  return signed_divide(numerator, denominator).quotient;
 }
 
 unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator) {
-  return divide(numerator, denominator).quotient;
+  return unsigned_divide(numerator, denominator).quotient;
 }
 
 void __aeabi_idivmod(int numerator, int denominator) {
-  struct divide_return ret = divide(numerator, denominator);
-  value_in_regs((int)ret.quotient, (int)ret.remainder);
+  struct signed_divide_return idr = signed_divide(numerator, denominator);
+  value_in_regs((int)idr.quotient, (int)idr.remainder);
 }
 
 void __aeabi_uidivmod(unsigned int numerator, unsigned int denominator) {
-  struct divide_return ret = divide(numerator, denominator);
-  value_in_regs((unsigned int)ret.quotient, (unsigned int)ret.remainder);
+  struct unsigned_divide_return udr = unsigned_divide(numerator, denominator);
+  value_in_regs((unsigned int)udr.quotient, (unsigned int)udr.remainder);
+}
+
+void __aeabi_ldivmod(long long numerator, long long denominator) {
+  struct signed_divide_return sdr = signed_divide(numerator, denominator);
+  value_in_regs((unsigned int)(sdr.quotient >> 32), (unsigned int)(sdr.quotient & 0xffffffffull), (unsigned int)(sdr.remainder >> 32), (unsigned int)(sdr.remainder & 0xffffffffull));
+}
+
+void __aeabi_uldivmod(unsigned long long numerator, unsigned long long denominator) {
+  struct unsigned_divide_return udr = unsigned_divide(numerator, denominator);
+  value_in_regs((unsigned int)(udr.quotient >> 32), (unsigned int)(udr.quotient & 0xffffffffull), (unsigned int)(udr.remainder >> 32), (unsigned int)(udr.remainder & 0xffffffffull));
 }
--- tile/kernel/helpers.h
+++ tile/kernel/helpers.h
@@ -3,17 +3,26 @@
 
 extern void value_in_regs();
 
-struct divide_return {
+struct signed_divide_return {
+  long long quotient;
+  long long remainder;
+};
+
+struct unsigned_divide_return {
   unsigned long long quotient;
   unsigned long long remainder;
 };
 
-struct divide_return divide(unsigned long long numerator, unsigned long long denominator);
+static struct signed_divide_return signed_divide(long long numerator, long long denominator);
+static struct unsigned_divide_return unsigned_divide(unsigned long long numerator, unsigned long long denominator);
 
 int __aeabi_idiv(int numerator, int denominator);
 unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator);
 
 void __aeabi_idivmod(int numerator, int denominator);
 void __aeabi_uidivmod(unsigned int numerator, unsigned int denominator);
+
+void __aeabi_ldivmod(long long numerator, long long denominator);
+void __aeabi_uldivmod(unsigned long long numerator, unsigned long long denominator);
 
 #endif
