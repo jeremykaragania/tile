1e68607d923f1cc40ee0d84fbe7661264b95bc25
Jeremy Karagania <jeremykaragania@icloud.com>
2024-05-27 12:15:13 +0800

    Update compiler integer division helpers

--- tile/kernel/helpers.c
+++ tile/kernel/helpers.c
@@ -1,5 +1,17 @@
 #include <helpers.h>
 
+struct divide_return divide(unsigned long long numerator, unsigned long long denominator) {
+  struct divide_return ret = {
+    0,
+    numerator
+  };
+  while (ret.remainder >= denominator) {
+    ret.remainder -= denominator;
+    ret.quotient += 1;
+  }
+  return ret;
+}
+
 int __aeabi_idiv(int numerator, int denominator) {
   int sign = 0;
   int q;
@@ -22,14 +34,12 @@ unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator) {
   return divide(numerator, denominator).quotient;
 }
 
-struct divide_return divide(unsigned long long numerator, unsigned long long denominator) {
-  struct divide_return ret = {
-    0,
-    numerator
-  };
-  while (ret.remainder >= denominator) {
-    ret.remainder -= denominator;
-    ret.quotient += 1;
-  }
-  return ret;
+void __aeabi_idivmod(int numerator, int denominator) {
+  struct divide_return ret = divide(numerator, denominator);
+  value_in_regs((int)ret.quotient, (int)ret.remainder);
+}
+
+void __aeabi_uidivmod(unsigned int numerator, unsigned int denominator) {
+  struct divide_return ret = divide(numerator, denominator);
+  value_in_regs((unsigned int)ret.quotient, (unsigned int)ret.remainder);
 }
--- tile/kernel/helpers.h
+++ tile/kernel/helpers.h
@@ -1,6 +1,8 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+extern void value_in_regs();
+
 struct divide_return {
   unsigned long long quotient;
   unsigned long long remainder;
@@ -10,5 +12,8 @@ struct divide_return divide(unsigned long long numerator, unsigned long long den
 
 int __aeabi_idiv(int numerator, int denominator);
 unsigned int __aeabi_uidiv(unsigned int numerator, unsigned int denominator);
+
+void __aeabi_idivmod(int numerator, int denominator);
+void __aeabi_uidivmod(unsigned int numerator, unsigned int denominator);
 
 #endif
