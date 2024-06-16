f82ff3fe87ad585142569f8f79fe9deaba3108fe
Jeremy Karagania <jeremykaragania@icloud.com>
2024-06-16 16:56:47 +0800

    Fix memory management information

--- tile/kernel/main.c
+++ tile/kernel/main.c
@@ -12,7 +12,7 @@ char tile_banner[] = "Tile\n";
 void start_kernel() {
   disable_interrupts();
   set_process_stack_end_token(&init_process);
-  init_memory_manager((uint32_t*)PG_DIR_PADDR, &text_begin, &text_end, &data_begin, &data_end);
+  init_memory_manager((uint32_t*)phys_to_virt(PG_DIR_PADDR), &text_begin, &text_end, &data_begin, &data_end, &bss_begin, &bss_end);
   init_memory_map();
   update_memory_map();
   init_paging();
