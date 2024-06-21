6efc7a8ea1a180ae1b196f8db5661be840dd2f3b
Jeremy Karagania <jeremykaragania@icloud.com>
2024-06-21 13:06:13 +0800

    Update paging semantics

--- tile/kernel/page.c
+++ tile/kernel/page.c
@@ -17,12 +17,12 @@ void init_paging() {
 void init_pgd() {
   /* Clear page table entries below the kernel. */
   for (size_t i = 0; i < (uint32_t)&VIRT_OFFSET; i += PTE_SIZE) {
-    pte_clear(&memory_manager, i);
+    pmd_clear(&memory_manager, i);
   }
 
   /* Clear page table entries above the kernel. */
   for (size_t i = high_memory; i < VMALLOC_BEGIN_VADDR; i += PTE_SIZE) {
-    pte_clear(&memory_manager, i);
+    pmd_clear(&memory_manager, i);
   }
 }
 
@@ -40,15 +40,15 @@ void map_kernel() {
 }
 
 /*
-  map_smc maps the static memory controller. It allocates a page table entry
-  and maps the UART from 0x1c090000 to 0xffc00000.
+  map_smc maps the static memory controller. It allocates a page middle
+  directory and maps the UART from 0x1c090000 to 0xffc00000.
 */
 void map_smc() {
-  uint32_t* pte;
+  uint32_t* pmd;
 
   uart_0 = (volatile struct uart_registers*)0xffc00000;
-  pte = pte_alloc(&memory_manager, 0xffc00000);
-  pte_insert(pte, 0xffc00000, 0x1c090000);
+  pmd = pmd_alloc(&memory_manager, 0xffc00000);
+  pmd_insert(pmd, 0xffc00000, 0x1c090000);
 }
 
 /*
@@ -56,72 +56,73 @@ void map_smc() {
   the physical address "p_addr" spanning "size" bytes.
 */
 void create_mapping(uint32_t v_addr, uint64_t p_addr, uint32_t size) {
-  uint32_t* pte;
+  uint32_t* pmd;
 
   for (size_t i = v_addr; i < v_addr + size; i += PTE_SIZE) {
-    pte = pgd_offset(memory_manager.pgd, i);
+    pmd = pgd_offset(memory_manager.pgd, i);
 
-    /* Use many PAGE_SIZE entries. */
+    /* Page middle directory has many entries. */
     if (!IS_ALIGNED(i, PTE_SIZE) || i + PTE_SIZE > v_addr + size) {
-      if (!pte_is_page_table(pte)) {
-        pte = pte_alloc(&memory_manager, i);
+      if (!pmd_is_page_table(pmd)) {
+        pmd = pmd_alloc(&memory_manager, i);
       }
       else {
-        pte = pte_to_page_table(pte);
+        pmd = pmd_to_page_table(pmd);
       }
 
       for (size_t j = i; j < v_addr + size; j += PAGE_SIZE, p_addr += PAGE_SIZE) {
-        pte_insert(pte, j, p_addr);
+        pmd_insert(pmd, j, p_addr);
       }
     }
-    /* Use one PTE_SIZE entry. */
+    /* Page middle directory has one entry. */
     else {
-      *pte = i | 0x402;
+      *pmd = i | 0x402;
     }
   }
 }
 
 /*
-  pgd_offset returns the address of a page table entry from a virtual address
-  "addr", and the base address of a page global directory "pgd". In ARMv7-A
-  parlance, it returns the address of a page table first-level descriptor from
-  a virtual address "addr", and a translation table base, "pgd".
+  pgd_offset returns the address of a page middle directory from a virtual
+  address "addr", and the base address of a page global directory "pgd". In
+  ARMv7-A parlance, it returns the address of a page table first-level
+  descriptor from a virtual address "addr", and a translation table base,
+  "pgd".
 */
 uint32_t* pgd_offset(uint32_t* pgd, uint32_t addr) {
   return (uint32_t*)((uint32_t)pgd + (uint32_t)pgd_index(addr));
 }
 
 /*
-  pte_offset returns the address of a page from a virtual address "addr", and
-  the base address of a page table entry "pte". In ARMv7-A parlance, it returns
-  the address of a small page second-level descriptor from a virtual address,
-  "addr" and a page table base address, "pte".
+  pmd_offset returns the address of a page table entry from a virtual address
+  "addr", and the base address of a page middle directory "pmd". In ARMv7-A
+  parlance, it returns the address of a small page second-level descriptor from
+  a virtual address, "addr" and a page table base address, "pmd".
 */
-uint32_t* pte_offset(uint32_t* pte, uint32_t addr) {
-  return (uint32_t*)((uint32_t)pte + (uint32_t)pte_index(addr));
+uint32_t* pmd_offset(uint32_t* pmd, uint32_t addr) {
+  return (uint32_t*)((uint32_t)pmd + (uint32_t)pmd_index(addr));
 }
 
 /*
-  pte_alloc allocates a new page table entry from a virtual address "addr" and
-  returns a pointer to it. The appropriate index in the page global directory
-  specified by the memory manager "mm" is updated to point to the new page
-  table entry.
+  pmd_alloc allocates a new page middle directory from a virtual address "addr"
+  and returns a pointer to it. The appropriate index in the page global
+  directory specified by the memory manager "mm" is updated to point to the new
+  page middle directory.
 */
-uint32_t* pte_alloc(struct memory_manager* mm, uint32_t addr) {
-  uint32_t* pte;
+uint32_t* pmd_alloc(struct memory_manager* mm, uint32_t addr) {
+  uint32_t* pmd;
   uint32_t* offset;
 
-  pte = memory_alloc(PAGE_SIZE);
+  pmd = memory_alloc(PAGE_SIZE);
   offset = pgd_offset(mm->pgd, addr);
-  *offset = ((uint32_t)virt_to_phys((uint32_t)pte) & 0xfffffc00) | 0x1;
-  return pte;
+  *offset = ((uint32_t)virt_to_phys((uint32_t)pmd) & 0xfffffc00) | 0x1;
+  return pmd;
 }
 
 /*
-  pte_clear clears a page table entry from a virtual address "addr" in the page
-  global directory specified by the memory manager "mm".
+  pmd_clear clears a page middle directory from a virtual address "addr" in the
+  page global directory specified by the memory manager "mm".
 */
-void pte_clear(struct memory_manager* mm, uint32_t addr) {
+void pmd_clear(struct memory_manager* mm, uint32_t addr) {
   uint32_t* offset;
 
   offset = pgd_offset(mm->pgd, addr);
@@ -129,32 +130,33 @@ void pte_clear(struct memory_manager* mm, uint32_t addr) {
 }
 
 /*
-  pte_insert inserts a page into a page table entry "pte". The page is
-  specified by which physical address "p_addr" it maps to which virtual address
-  "v_addr". If a page already exists it is replaced.
+  pmd_insert inserts a page table entry into a page middle directory "pmd". The
+  page table entry is specified by which physical address "p_addr" it maps to
+  which virtual address "v_addr". If a page table entry already exists it is
+  replaced.
 */
-void pte_insert(uint32_t* pte, uint32_t v_addr, uint64_t p_addr) {
+void pmd_insert(uint32_t* pmd, uint32_t v_addr, uint64_t p_addr) {
   uint32_t* offset;
 
-  offset = pte_offset(pte, v_addr);
+  offset = pmd_offset(pmd, v_addr);
   *offset = ((uint32_t)p_addr & 0xfffff000) | 0x1 << 0x4 | 0x1 << 0x1;
 }
 
 /*
-  pte_is_page_table returns a positive integer if the page table entry "pte" is
-  a page table.
+  pmd_is_page_table returns a positive integer if the page middle directory
+  "pmd" is a page table.
 */
-int pte_is_page_table(uint32_t* pte) {
-  return *pte & 1;
+int pmd_is_page_table(uint32_t* pmd) {
+  return *pmd & 1;
 }
 
 /*
-  pte_to_page_table returns the page table address of a page table entry "pte"
-  if there's one.
+  pmd_to_page_table returns the page table address of a page middle directory
+  "pmd" if there's one.
 */
-uint32_t* pte_to_page_table(uint32_t* pte) {
-  if (pte_is_page_table(pte)) {
-    return (uint32_t*)phys_to_virt(*pte & 0xfffff000);
+uint32_t* pmd_to_page_table(uint32_t* pmd) {
+  if (pmd_is_page_table(pmd)) {
+    return (uint32_t*)phys_to_virt(*pmd & 0xfffff000);
   }
   return NULL;
 }
--- tile/kernel/page.h
+++ tile/kernel/page.h
@@ -6,7 +6,7 @@
 #include <uart.h>
 
 extern uint32_t pgd_index(uint32_t addr);
-extern uint32_t pte_index(uint32_t addr);
+extern uint32_t pmd_index(uint32_t addr);
 extern void invalidate_entire_tlb();
 
 void init_paging();
@@ -18,12 +18,12 @@ void map_smc();
 void create_mapping(uint32_t v_addr, uint64_t p_addr, uint32_t size);
 
 uint32_t* pgd_offset(uint32_t* pgd, uint32_t addr);
-uint32_t* pte_offset(uint32_t* pte, uint32_t addr);
-uint32_t* pte_alloc(struct memory_manager* mm, uint32_t addr);
-void pte_clear(struct memory_manager* mm, uint32_t addr);
-void pte_insert(uint32_t* pte, uint32_t v_addr, uint64_t p_addr);
+uint32_t* pmd_offset(uint32_t* pmd, uint32_t addr);
+uint32_t* pmd_alloc(struct memory_manager* mm, uint32_t addr);
+void pmd_clear(struct memory_manager* mm, uint32_t addr);
+void pmd_insert(uint32_t* pmd, uint32_t v_addr, uint64_t p_addr);
 
-uint32_t* pte_to_page_table(uint32_t* pte);
-int pte_is_page_table(uint32_t* pte);
+int pmd_is_page_table(uint32_t* pmd);
+uint32_t* pmd_to_page_table(uint32_t* pmd);
 
 #endif
