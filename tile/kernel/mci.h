7a92ffb21331a3a0b5b59cf48b329fcbc2ab57a9
Jeremy Karagania <jeremykaragania@icloud.com>
2024-07-22 19:04:05 +0800

    Update multimedia card interface registers

--- tile/kernel/mci.h
+++ tile/kernel/mci.h
@@ -12,31 +12,21 @@ struct mci_registers {
   uint32_t argument;
   uint32_t command;
   uint32_t resp_cmd;
-  uint32_t response_0;
-  uint32_t response_1;
-  uint32_t response_2;
-  uint32_t response_3;
+  uint32_t response[4];
   uint32_t data_timer;
   uint32_t data_length;
   uint32_t data_ctrl;
   uint32_t data_cnt;
   uint32_t status;
   uint32_t clear;
-  uint32_t mask_0;
-  uint32_t mask_1;
+  uint32_t mask[2];
   uint32_t select;
   uint32_t fifo_cnt;
   uint32_t reserved_0[13];
   uint32_t fifo[16];
   uint32_t reserved_1[968];
-  uint32_t periph_id_0;
-  uint32_t periph_id_1;
-  uint32_t periph_id_2;
-  uint32_t periph_id_3;
-  uint32_t p_cell_d_0;
-  uint32_t p_cell_d_1;
-  uint32_t p_cell_d_2;
-  uint32_t p_cell_d_3;
+  uint32_t periph_id[4];
+  uint32_t p_cell_d[4];
 };
 
 #endif
