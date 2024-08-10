7a9fb77dae1b815bd27659c5c40462358c6a91c9
Jeremy Karagania <jeremykaragania@icloud.com>
2024-08-10 16:50:57 +0800

    Update filesystem file information

--- tile/kernel/file.c
+++ tile/kernel/file.c
@@ -1,12 +1,29 @@
 #include <kernel/file.h>
 
-struct file_info file_table[32];
-struct inode inode_table[32];
+struct file_entry file_table[FILE_TABLE_SIZE];
+struct file_info_entry file_info_table[FILE_INFO_TABLE_SIZE];
 
-struct inode* lookup_inode(const char* name) {
-  for (size_t i = 0; i < 32; ++i) {
-    if (strcmp(inode_table[i].name, name) == 0) {
-      return &inode_table[i];
+/*
+  lookup_file_info searches the file information table "file_info_table" by a
+  filename and returns the file's information.
+*/
+struct file_info_entry* lookup_file_info(const char* filename) {
+  for (size_t i = 0; i < FILE_INFO_TABLE_SIZE; ++i) {
+    struct file_info_entry entry = file_info_table[i];
+
+    if (entry.type == FT_DIRECTORY) {
+      char block[FILE_BLOCK_SIZE];
+
+      mci_read(entry.blocks[0], block, 512);
+
+      for (size_t j = (uint32_t)block; j < (uint32_t)block + entry.size; j += 16) {
+        size_t number = *(uint16_t*)((char*)j);
+        char* name = (char*)(j + 2);
+
+        if (strcmp(name, filename) == 0) {
+          return &file_info_table[number];
+        }
+      }
     }
   }
 
--- tile/kernel/file.h
+++ tile/kernel/file.h
@@ -1,29 +1,39 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <kernel/mci.h>
 #include <lib/string.h>
 #include <stddef.h>
+#include <stdint.h>
 
-enum connection_status {
-  FILE_READ,
-  FILE_WRITE
+#define FILE_TABLE_SIZE 32
+#define FILE_INFO_TABLE_SIZE 32
+#define FILE_BLOCK_SIZE 512
+
+enum file_status {
+  FS_READ,
+  FS_WRITE
 };
 
-struct file_info {
+enum file_type {
+  FT_REGULAR,
+  FT_DIRECTORY,
+  FT_CHARACTER,
+  FT_BLOCK,
+  FT_FIFO
+};
+
+struct file_entry {
   int status;
   size_t offset;
-  struct inode* inode;
 };
 
-struct inode {
-  char* name;
+struct file_info_entry {
+  uint32_t blocks[15];
+  int type;
+  size_t size;
 };
 
-struct directory_entry {
-  char* filename;
-  struct inode* inode;
-};
-
-struct inode* lookup_inode(const char* name);
+struct file_info_entry* lookup_file_info(const char* name);
 
 #endif
