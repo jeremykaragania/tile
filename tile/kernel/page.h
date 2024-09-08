6543bfedf44d448ef380607ac5821abec99c5181
Jeremy Karagania <jeremykaragania@icloud.com>
2024-09-08 17:16:12 +0800

    Fix page bitmap

--- tile/kernel/page.h
+++ tile/kernel/page.h
@@ -12,7 +12,7 @@
 #define pmd_index_abs(addr) ((addr & PAGE_MASK) >> PAGE_SHIFT)
 #define pgd_index(addr) (4 * pgd_index_abs(addr))
 #define pmd_index(addr) (4 * pmd_index_abs(addr))
-#define page_index(addr) (pgd_index_abs(addr) * PAGE_SIZE + pmd_index_abs(addr))
+#define page_index(addr) (pgd_index_abs(addr) * PAGES_PER_PAGE_TABLE + pmd_index_abs(addr))
 #define page_bitmap_index(addr) (page_index(addr) / 32)
 #define page_bitmap_index_index(addr) (pmd_index_abs(addr) % 32)
 
