da7f88b8af62085d60f9c12b10553ace13398bd6
Jeremy Karagania <jeremykaragania@icloud.com>
2024-09-08 18:19:26 +0800

    Add missing file

--- tile/kernel/asm/page.h
+++ tile/kernel/asm/page.h
@@ -1,9 +1,12 @@
 #ifndef ASM_PAGE_H
 #define ASM_PAGE_H
 
+#define VADDR_SPACE_SIZE 0x100000000ull
+
 #define PMD_SIZE 0x100000
 #define PAGE_SIZE 0x1000
 #define PAGE_TABLE_SIZE 0x400
+#define PAGES_PER_PAGE_TABLE (PAGE_TABLE_SIZE / 4)
 
 #define PG_DIR_SHIFT 0x14
 #define PAGE_SHIFT 0xc
