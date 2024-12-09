545f3c6c6a9309f405cd1832a5f45005da52a22c
Jeremy Karagania <jeremykaragania@icloud.com>
2024-12-09 18:17:11 +0800

    Rename identifiers

--- tile/kernel/process.c
+++ tile/kernel/process.c
@@ -1,25 +1,25 @@
 #include <kernel/process.h>
 
-struct process init_process __attribute__((section(".init_process"))) = {
+struct process_info init_process __attribute__((section(".init_process"))) = {
   0,
-  CREATED,
+  PS_CREATED,
   &init_process_stack
 };
 
 /*
-  set_process_stack_end_token receives a struct process "proc", and adds a
+  set_process_stack_end_token receives a struct process_info "proc", and adds a
   magic token to the end of its stack.  This allows us to detect the last
   usable uint32_t of the stack.
 */
-void set_process_stack_end_token(const struct process* proc) {
+void set_process_stack_end_token(const struct process_info* proc) {
   *(uint32_t*)(proc + 0x1) = STACK_END_MAGIC;
 }
 
 /*
-  current_process returns the current struct process in use. A struct process
-  is always to a THREAD_SIZE boundary. From the current stack pointer, we can
-  find the related struct process.
+  current_process returns the current struct process in use. A struct
+  process_info is always to a THREAD_SIZE boundary. From the current stack
+  pointer, we can find the related struct process.
 */
-struct process* current_process() {
-  return (struct process*) (current_stack_pointer() & ~(THREAD_SIZE - 1));
+struct process_info* current_process() {
+  return (struct process_info*) (current_stack_pointer() & ~(THREAD_SIZE - 1));
 }
--- tile/kernel/process.h
+++ tile/kernel/process.h
@@ -10,30 +10,30 @@ extern void* init_process_begin;
 extern void* init_process_end;
 extern void* init_process_stack;
 
-extern struct process init_process;
+extern struct process_info init_process;
 extern uint32_t current_stack_pointer();
 
 /*
   enum process_state represents the state of a process.
 */
 enum process_state  {
-  CREATED,
-  READY,
-  RUNNING,
-  BLOCKED,
-  TERMINATED
+  PS_CREATED,
+  PS_READY,
+  PS_RUNNING,
+  PS_BLOCKED,
+  PS_TERMINATED
 };
 
 /*
-  struct process represents a process.
+  struct process_info represents a process.
 */
-struct process {
+struct process_info {
   int id;
   int state;
   void* stack;
 };
 
-void set_process_stack_end_token(const struct process* proc);
-struct process* current_process();
+void set_process_stack_end_token(const struct process_info* proc);
+struct process_info* current_process();
 
 #endif
