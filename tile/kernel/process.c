5f50d43537d2566fce45a71220c53dd13eb0a1f8
Jeremy Karagania <jeremykaragania@icloud.com>
2025-01-29 17:37:32 +0800

    Fix stack end token

--- tile/kernel/process.c
+++ tile/kernel/process.c
@@ -15,11 +15,11 @@ struct process_info init_process __attribute__((section(".init_process"))) = {
 
 /*
   set_process_stack_end_token receives a struct process_info "proc", and adds a
-  magic token to the end of its stack.  This allows us to detect the last
-  usable uint32_t of the stack.
+  magic token to the end of its stack. This allows us to detect the last usable
+  uint32_t of the stack.
 */
 void set_process_stack_end_token(const struct process_info* proc) {
-  *(uint32_t*)(proc + 0x1) = STACK_END_MAGIC;
+  *(uint32_t*)(proc->stack) = STACK_END_MAGIC;
 }
 
 /*
