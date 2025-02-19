26b526fbcaa4970b43f7b3e35440654f4248d5a3
Jeremy Karagania <jeremykaragania@icloud.com>
2025-02-19 19:55:17 +0800

    Update global interrupt controller addresses

--- tile/drivers/gic_400.c
+++ tile/drivers/gic_400.c
@@ -1,4 +1,4 @@
 #include <drivers/gic_400.h>
 
-volatile struct gic_distributor_registers* gicd = (volatile struct gic_distributor_registers*)(GIC_400_PADDR + 0x1000);
-volatile struct gic_cpu_interface_registers* gicc = (volatile struct gic_cpu_interface_registers*)(GIC_400_PADDR + 0x2000);
+volatile struct gic_distributor_registers* gicd = (volatile struct gic_distributor_registers*)GICD_PADDR;
+volatile struct gic_cpu_interface_registers* gicc = (volatile struct gic_cpu_interface_registers*)GICC_PADDR;
