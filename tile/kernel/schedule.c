2f2f2410dd5f3c1072f88326ef270ea7482f3bbb
Jeremy Karagania <jeremykaragania@icloud.com>
2025-07-07 07:00:59 -0400

    Update scheduling

--- tile/kernel/schedule.c
+++ tile/kernel/schedule.c
@@ -8,3 +8,18 @@ struct process_info* schedule_queue[SCHEDULE_QUEUE_SIZE];
 void schedule_init() {
   schedule_queue[0] = &init_process;
 }
+
+/*
+  schedule schedules the next process to be executed and context switches to
+  it.
+*/
+void schedule() {
+  struct process_info* curr = current;
+  struct process_info* next = current->next;
+
+  if (next == &process_infos) {
+    next = next->next;
+  }
+
+  context_switch(&current->reg, &next->reg);
+}
--- tile/kernel/schedule.h
+++ tile/kernel/schedule.h
@@ -6,6 +6,7 @@
 #define SCHEDULE_QUEUE_SIZE 32
 
 void schedule_init();
+void schedule();
 
 extern struct process_info* schedule_queue[SCHEDULE_QUEUE_SIZE];
 extern void context_switch(struct processor_registers* from, struct processor_registers *to);
