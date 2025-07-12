45e80b54eea2564006543761ae4875f528132d72
Jeremy Karagania <jeremykaragania@icloud.com>
2025-07-11 22:21:15 -0400

    Fix dual timer initialization

--- tile/drivers/sp804.c
+++ tile/drivers/sp804.c
@@ -3,6 +3,11 @@
 volatile struct dual_timer_registers* timer_0 = (struct dual_timer_registers*)TIMER_1_PADDR;
 
 void dual_timer_init() {
-  timer_0->timer1_control = 1 << 6;
-  timer_0->timer1_control = 1 << 7;
+  timer_0->timer1_load = 0xfffe;
+
+  timer_0->timer1_control = 0;
+  timer_0->timer1_control |= 1 << 1;
+  timer_0->timer1_control |= 1 << 5;
+  timer_0->timer1_control |= 1 << 6;
+  timer_0->timer1_control |= 1 << 7;
 }
