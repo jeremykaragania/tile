82d77b3a6d00d378a379ac523d2b95e3782d0ef4
Jeremy Karagania <jeremykaragania@icloud.com>
2025-07-24 20:40:31 +0800

    Add removed paging helpers

--- tile/kernel/page.h
+++ tile/kernel/page.h
@@ -19,10 +19,13 @@ void map_vector_table();
 void map_smc();
 
 void* create_mapping(uint32_t v_addr, uint32_t p_addr, uint32_t size, int flags);
+int mapping_exists(uint32_t* pgd, uint32_t v_addr, uint32_t p_addr);
+uint32_t pgd_walk(uint32_t* pgd, uint32_t v_addr);
 
 uint32_t* addr_to_pmd(uint32_t* pgd, uint32_t addr);
 uint32_t* addr_to_pte(uint32_t* pmd, uint32_t addr);
 uint32_t pmd_to_addr(uint32_t* pgd, uint32_t* pmd);
+uint32_t pmd_section_to_addr(uint32_t pmd);
 uint32_t pte_to_addr(uint32_t pte);
 
 void pmd_clear(uint32_t* pgd, uint32_t addr);
