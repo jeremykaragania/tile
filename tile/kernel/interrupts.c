ae615d057b4590addb36553e8954137805a1da4d
Jeremy Karagania <jeremykaragania@icloud.com>
2025-08-22 22:47:00 +0800

    Fix demand paging

--- tile/kernel/interrupts.c
+++ tile/kernel/interrupts.c
@@ -31,7 +31,6 @@ void do_prefetch_abort() {}
 */
 void do_data_abort() {
   uint32_t dfar = get_dfar();
-  uint32_t* pmd = addr_to_pmd(current->mem->pgd, dfar);
   struct list_link* pages_head = &current->mem->pages_head;
   struct page_region* region;
 
@@ -39,7 +38,7 @@ void do_data_abort() {
 
   /* The page region isn't backed by a file. */
   if (!region->file_int) {
-    pmd_insert(pmd, dfar, (uint32_t)page_group_alloc(page_groups, PHYS_OFFSET, 1, 1, 0), region->flags);
+    create_mapping(dfar, (uint32_t)page_group_alloc(page_groups, PHYS_OFFSET, 1, 1, 0), PAGE_SIZE, region->flags);
   }
 }
 
