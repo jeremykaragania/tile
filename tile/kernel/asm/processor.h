35caf4503658527cf7520d134e157f9b577ae16e
Jeremy Karagania <jeremykaragania@icloud.com>
2025-10-06 12:46:45 +0800

    Add header guard

--- tile/kernel/asm/processor.h
+++ tile/kernel/asm/processor.h
@@ -1,3 +1,6 @@
+#ifndef ASM_PROCESSOR_H
+#define ASM_PROCESSOR_H
+
 /* Processor modes. */
 #define PM_USR 0x10
 #define PM_FIQ 0x11
@@ -8,3 +11,5 @@
 #define PM_HYP 0x1a
 #define PM_UND 0x1b
 #define PM_SYS 0x1f
+
+#endif
