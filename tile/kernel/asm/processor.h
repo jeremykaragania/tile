eb11c8682bb1cda8286200d461a278a1fd7f7830
Jeremy Karagania <jeremykaragania@icloud.com>
2025-10-07 21:45:06 +0800

    Add processor register indexes

--- tile/kernel/asm/processor.h
+++ tile/kernel/asm/processor.h
@@ -12,4 +12,23 @@
 #define PM_UND 0x1b
 #define PM_SYS 0x1f
 
+/* Processor register indexes. */
+#define PR_R0 0
+#define PR_R1 1
+#define PR_R2 2
+#define PR_R3 3
+#define PR_R4 4
+#define PR_R5 5
+#define PR_R6 6
+#define PR_R7 7
+#define PR_R8 8
+#define PR_R9 9
+#define PR_R10 10
+#define PR_R11 11
+#define PR_R12 12
+#define PR_SP 13
+#define PR_LR 14
+#define PR_PC 15
+#define PR_CPSR 16
+
 #endif
