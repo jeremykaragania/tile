ddd4ab230883fe09f822071a313f1a3ca7b828c0
Jeremy Karagania <jeremykaragania@icloud.com>
2025-12-09 23:03:01 +0800

    Add interrupt IDs

--- tile/kernel/interrupts.h
+++ tile/kernel/interrupts.h
@@ -4,7 +4,45 @@
 #include <stdint.h>
 #include <kernel/processor.h>
 
+// Watchdog timer.
+#define WDOG0INT 32
+
+// Software interrupts.
+#define SWINT 33
+
+// Timer interrupts.
 #define TIM01INT 34
+#define TIM23INT 35
+#define RTCINTR 36
+
+// UART interrupts.
+#define UART0INTR 37
+#define UART1INTR 38
+#define UART2INTR 39
+#define UART3INTR 40
+
+// MultiMedia card interrupts.
+#define MCI_INTR_0 41
+#define MCI_INTR_1 42
+
+// Audio CODEC interrupts.
+#define AACI_INTR 43
+
+// Keyboard/Mouse interrupts.
+#define KMI0_INTR 44
+#define KMI1_INTR 45
+
+// Display interrupts.
+#define CLCDINTR 46
+
+// Ethernet interrupts.
+#define ETH_INTR 47
+
+// USB interrupts.
+#define USB_INT 48
+
+// PCI-Express interrupts.
+#define PCIE_GPEN 49
 
 int handle_fault(uint32_t addr);
 
