667c3a8eacee83c0b2b08884d308a8072cfc423a
Jeremy Karagania <jeremykaragania@icloud.com>
2025-12-19 12:22:47 +0800

    Add strcpy declaration

--- tile/lib/string.h
+++ tile/lib/string.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+char* strcpy(char *dest, const char *src);
 size_t strlen(const char* s);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
